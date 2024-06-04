1bc57719ca550f7edc90db422a6a921a53dd1ee0
Denys <denis.gidzun@gmail.com>
2024-06-04 20:22:28 +0300

    ctn

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    [SerializeField]private float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
