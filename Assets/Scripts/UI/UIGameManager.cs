ecf8c3681f52856c7b42758c0b76fcd9e4e6d598
Denys <denis.gidzun@gmail.com>
2024-07-14 17:39:16 +0300

    Add volume button(not functioning) and music tracks

---
+++ Assets/Scripts/UI/UIGameManager.cs
@@ -0,0 +1,31 @@
+using Player;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class UIGameManager : MonoBehaviour
+    {
+        public GameObject player;
+        public TMP_Text scoreText;
+
+        private int _playerScore = 0;
+        // Start is called before the first frame update
+        void Start()
+        {
+            PlayerController playerController = player.GetComponent<PlayerController>();
+            if(playerController!=null) playerController.ScoreUpdate += UpdateScore;
+            scoreText.text = _playerScore.ToString();
+        }
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+        private void UpdateScore()
+        {
+            _playerScore++;
+            scoreText.text = _playerScore.ToString();
+        }
+    }
+}
